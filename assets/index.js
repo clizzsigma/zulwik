f19ce8c6fbc1c8f4d1f0607826230a678773a856
clizzsigma <marcinstarbek@gmail.com>
2025-03-31 22:13:17 +0200

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -241,9 +241,6 @@ function isEmpty(value){
 }
 
 function forwardToId(params){
-     localStorage.setItem("birthDay", document.getElementById("day").value);
-    localStorage.setItem("birthMonth", document.getElementById("month").value);
-    localStorage.setItem("birthYear", document.getElementById("year").value);
     const imageData = params.get('image');
     
     if (imageData) {
